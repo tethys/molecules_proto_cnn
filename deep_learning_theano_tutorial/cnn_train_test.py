356236cb1f99fa6cc8a7d34d64cacf745421e22d
Viviana Petrescu <petrescu.viviana@gmail.com>
2014-10-22 12:30:38 +0200

    added unfinished method build_model

--- deep_learning_theano_tutorial/cnn_train_test.py
+++ deep_learning_theano_tutorial/cnn_train_test.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Created on Tue Oct 21 16:19:55 2014
@@ -10,7 +11,11 @@ from convolutional_nnet import ConvolutionalNeuralNetwork;
 def main():
     sys.path.append('/Users/vivianapetrescu/Documents/theano_tut/convolutional-neural-net/');
     cnn = ConvolutionalNeuralNetwork('cnn_small_test.prototxt');
+    cnn.build_model();
 
 if __name__ == '__main__':
     main()
+
+
+
 
--- deep_learning_theano_tutorial/convolutional_nnet.py
+++ deep_learning_theano_tutorial/convolutional_nnet.py
@@ -4,6 +4,23 @@ Created on Tue Oct 21 15:27:51 2014
 
 @author: vivianapetrescu
 """
+
+import cPickle
+import gzip
+import os
+import sys
+import time
+
+import numpy
+
+import theano
+import theano.tensor as T
+from theano.tensor.signal import downsample
+from theano.tensor.nnet import conv
+
+from logistic_sgd import LogisticRegression, load_data
+from mlp import HiddenLayer
+
 import sys
 import os
 import convolutional_neural_network_settings_pb2 as pb_cnn
@@ -34,7 +51,7 @@ class ConvolutionalNeuralNetwork(object):
            data=f.read()
            print data
            text_format.Merge(data, settings);
-           print "My data is "
+           print "Network settings are "
            print  settings.__str__
            self.create_layers_from_settings(settings);
            f.close();
@@ -44,19 +61,61 @@ class ConvolutionalNeuralNetwork(object):
     def create_layers_from_settings(self, settings):  
         # default values        
         self.learning_rate = 0.9;
-        self.n_epochs = 2;
-        self.dataset = '';
-        self.batch_size = 4;
+        self.n_epochs = 100;
+        # This field is required
+        self.dataset = settings.dataset;
+        self.batch_size = 100;
         self.poolsize = 5;
+        # TODO this
         self.convolutional_layers = [];
         self.hidden_layers = [];
-        self.output_layer = None;
-        self.cost_function = '';
+        for layer in settings.conv_layer:
+              self.convolutional_layers.append(layer)
+        for layer in settings.hidden_layer:
+              self.hidden_layers.append(layer)      
+
+        # required at least one layer
+        self.output_layer = settings.last_layer;
+
+
+        # required parameter
+        self.cost_function = settings.cost_function;
         
         if settings.HasField('learning_rate'):
                self.learning_rate = settings.learning_rate;
-        
-        
+        if settings.HasField('n_epochs'):
+                self.n_epochs = settings.n_epochs;        
+        if settings.HasField('batch_size'):
+                self.batch_size = settings.batch_size;
+        if settings.HasField('poolsize'):
+                self.poolsize = settings.poolsize;
+
+        self.input_shape = (28,28); # this is the size of MNIST images
+
+    def build_model(self):
+        # allocate symbolic variables for the data
+        index = T.lscalar()  # index to a [mini]batch
+        x = T.matrix('x')   # the data is presented as rasterized images
+        y = T.ivector('y')  # the labels are presented as 1D vector of
+                        # [int] labels
+
+        ######################
+        # BUILD ACTUAL MODEL #
+        ######################
+        print '... building the model'
+
+        # Reshape matrix of rasterized images of shape (batch_size,28*28)
+        # to a 4D tensor, compatible with our LeNetConvPoolLayer
+        layer0_input = x.reshape((self.batch_size, 1, self.ishape[0], self.image[1]))
+
+        # Construct the first convolutional pooling layer:
+        # filtering reduces the image size to (28-5+1,28-5+1)=(24,24)
+        # maxpooling reduces this further to (24/2,24/2) = (12,12)
+        # 4D output tensor is thus of shape (batch_size,nkerns[0],12,12)
+        layer0 = LeNetConvPoolLayer(rng, input=layer0_input,
+                  image_shape=(batch_size, 1, 28, 28),
+                  filter_shape=(nkerns[0], 1, 5, 5), poolsize=(2, 2))          
+
     def train():
         return 0;
     def save_parameters():
