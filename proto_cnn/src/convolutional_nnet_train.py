5de21fab88011727894f1c4ab0f0a5d77ef7f409
Viviana Petrescu <petrescu.viviana@gmail.com>
2014-12-17 16:00:09 +0100

    get the image shape from the loaded data

--- proto_cnn/src/convolutional_nnet_train.py
+++ proto_cnn/src/convolutional_nnet_train.py
@@ -20,7 +20,7 @@ from lenet_conv_pool_layer import LeNetConvPoolLayer
 import convolutional_neural_network_settings_pb2 as pb_cnn
 from google.protobuf import text_format
 # Parameters: make this a protocol buffer
-#  learning_rate=0.1, 
+#  learning_rate=0.1,
 #  n_epochs=2,
 #  dataset='mnist.pkl.gz',
 #  batch_size=50
@@ -32,68 +32,61 @@ from google.protobuf import text_format
 #  Layer3 full
 #     500 tanh
 #  Layer4 full - last
-#     10 
+#     10
 # Cost negative log likelihood
 
 # Make the network read this and setup
 class ConvolutionalNeuralNetworkTrain(object):
-    """ The class takes a proto bufer as input, setups a CNN according to the 
+    """ The class takes a proto bufer as input, setups a CNN according to the
         settings, trains the network and save the weights in a file
     """
     def __init__(self, cnn_settings_protofile, cached_weights_file):
-        # Create protobuff empty object    
+        # Create protobuff empty object
         self.cached_weights_file = cached_weights_file
         settings = pb_cnn.CNNSettings();
-        try:        
-           f = open(cnn_settings_protofile, "r")    
+        try:
+           f = open(cnn_settings_protofile, "r")
            data = f.read()
            text_format.Merge(data, settings);
            print "Network settings are \n"
            print settings.__str__
            print "\n"
-           
+
            # Build up the ConvolutionalNeuralNetworkTrain model
            self.create_layers_from_settings(settings);
            f.close();
         except IOError:
            print "Could not open file " + cnn_settings_protofile;
-        
-    def create_layers_from_settings(self, settings):  
-        # Default values for optionl parameters       
+
+    def create_layers_from_settings(self, settings):
+        # Default values for optionl parameters
         self.learning_rate = 1;
         self.n_epochs = 5;
         # This fields are required
         self.dataset = 'mnist.pkl.gz' #settings.dataset;
         self.batch_size = 50;
         self.poolsize = 2;
-        
+
         if settings.HasField('learning_rate'):
                 self.learning_rate = settings.learning_rate;
         if settings.HasField('n_epochs'):
-                self.n_epochs = settings.n_epochs;        
+                self.n_epochs = settings.n_epochs;
         if settings.HasField('batch_size'):
                 self.batch_size = settings.batch_size;
         if settings.HasField('poolsize'):
                 self.poolsize = settings.poolsize;
-        
-        self.learning_rate = 1;
-        self.n_epochs = 5;
+
         # This fields are required
         self.dataset = 'mnist.pkl.gz' #settings.dataset;
-        self.batch_size = 50;
-        self.poolsize = 2;
-             
+
+
         # TODO this
         self.convolutional_layers = [];
         self.hidden_layers = [];
-        
-       # self.nbr_convolutional_layers = settings.conv_layer.size();
-       # self.nbr_hidden_layers = settings.hidden_layer.size();
-      
         for layer in settings.conv_layer:
               self.convolutional_layers.append(layer)
         for layer in settings.hidden_layer:
-              self.hidden_layers.append(layer)      
+              self.hidden_layers.append(layer)
 
         # required at least one layer
         self.output_layer = settings.last_layer;
@@ -101,18 +94,22 @@ class ConvolutionalNeuralNetworkTrain(object):
         self.dataset = settings.dataset
         # required parameter
         self.cost_function = settings.cost_function;
-        
-        self.input_shape = (28,28); # this is the size of MNIST images
+
 
     def build_model(self):
         rng = numpy.random.RandomState(23455)
 
-        datasets = load_mnist_data_rescaled(self.dataset)
+        datasets = load_mnist_data(self.dataset)
 
         # Train, Validation, Test 50000, 10000, 10000 times 28x28 = 784
         self.train_set_x, self.train_set_y = datasets[0]
         self.valid_set_x, self.valid_set_y = datasets[1]
         self.test_set_x, self.test_set_y = datasets[2]
+
+        # assumes the width equals the height
+        img_width_size = numpy.sqrt(self.test_set_x.shape[1].eval()).astype(int)
+        print "Image shape ", img_width_size
+        self.input_shape = (img_width_size, img_width_size); # this is the size of MNIST images
 
         # compute number of minibatches for training, validation and testing
         # 50000/50 = 1000, 10000/50 = 200
@@ -138,33 +135,8 @@ class ConvolutionalNeuralNetworkTrain(object):
         print 'Building the model ...'
         for layer in self.convolutional_layers:
             print layer.num_filters
-            print layer.filter_w   
-            
-        # Reshape matrix of rasterized images of shape (batch_size,28*28)
-        # to a 4D tensor, compatible with our LeNetConvPoolLayer
-        # Construct the first convolutional pooling layer:
-        # filtering reduces the image size to (28-5+1,28-5+1)=(24,24)
-        #        # maxpooling reduces this further to (24/2,24/2) = (12,12)
-        #        # 4D output tensor is thus of shape (batch_size,nkerns[0],12,12)
-        #        layer0 = LeNetConvPoolLayer(rng, input=layer0_input,
-        #            image_shape=(self.batch_size, 1, 28, 28),
-        #            filter_shape=(nkerns[0], 1, 5, 5), poolsize=(2, 2))
-        #
-        #        # Construct the second convolutional pooling layer
-        #        # filtering reduces the image size to (12-5+1,12-5+1)=(8,8)
-        #        # maxpooling reduces this further to (8/2,8/2) = (4,4)
-        #        # 4D output tensor is thus of shape (nkerns[0],nkerns[1],4,4)
-        #
-        #        # layer0 output is a 4d tensor as well
-        #        layer1 = LeNetConvPoolLayer(rng, input=layer0.output,
-        #              image_shape=(self.batch_size, nkerns[0], 12, 12),
-        #              filter_shape=(nkerns[1], nkerns[0], 5, 5), poolsize=(2, 2))
-        #
-        #        # the HiddenLayer being fully-connected, it operates on 2D matrices of
-        #        # shape (batch_size,num_pixels) (i.e matrix of rasterized images).
-        #        # This will generate a matrix of shape (20,32*4*4) = (20,512)
-        #        layer2_input = layer1.output.flatten(2)
-                
+            print layer.filter_w
+
         layer_input = self.x.reshape((self.batch_size, 1, self.input_shape[0], self.input_shape[1]))
         pooled_W = self.input_shape[0];
         pooled_H = self.input_shape[1];
@@ -173,9 +145,9 @@ class ConvolutionalNeuralNetworkTrain(object):
         for clayer_params in self.convolutional_layers:
             print clayer_params.num_filters
             print clayer_params.filter_w
-            layer = LeNetConvPoolLayer(rng, input = layer_input, 
+            layer = LeNetConvPoolLayer(rng, input = layer_input,
                                        image_shape=(self.batch_size, nbr_feature_maps, pooled_W, pooled_H),
-                                       filter_shape=(clayer_params.num_filters, nbr_feature_maps, 
+                                       filter_shape=(clayer_params.num_filters, nbr_feature_maps,
                                                      clayer_params.filter_w, clayer_params.filter_w),
                                        poolsize=(self.poolsize, self.poolsize))
             clayers.append(layer)
@@ -183,8 +155,8 @@ class ConvolutionalNeuralNetworkTrain(object):
             pooled_H = (pooled_H - clayer_params.filter_w + 1) / self.poolsize;
             layer_input = layer.output;
             nbr_feature_maps = clayer_params.num_filters;
-        
-        
+
+
         # construct a fully-connected sigmoidal layer
         layer_input = layer_input.flatten(2);
         nbr_input = nbr_feature_maps * pooled_W * pooled_H ## Why is this SO??
@@ -196,7 +168,7 @@ class ConvolutionalNeuralNetworkTrain(object):
             nbr_input = hlayer_params.num_outputs;
             layer_input = layer.output
             hlayers.append(layer)
-            
+
         # classify the values of the fully-connected sigmoidal layer
         self.output_layer = LogisticRegression(input=layer_input, n_in= nbr_input, n_out=10)
 
@@ -204,15 +176,15 @@ class ConvolutionalNeuralNetworkTrain(object):
         self.cost = self.output_layer.negative_log_likelihood(self.y)
 
         # create a list of all model parameters to be fit by gradient descent
-        self.params = self.output_layer.params 
+        self.params = self.output_layer.params
         for hl in reversed(hlayers):
             self.params += hl.params
         for cl in reversed(clayers):
             self.params += cl.params
 
- 
+
         # create a list of gradients for all model parameters
-        self.grads = T.grad(self.cost, self.params)        
+        self.grads = T.grad(self.cost, self.params)
 
 
     def train_model(self):
@@ -280,8 +252,8 @@ class ConvolutionalNeuralNetworkTrain(object):
                   end = time.time()
                   mean_training_time += end - start
                   cnt_times += 1
-                                    
-                  
+
+
                   if (iter + 1) % validation_frequency == 0:
 
                     # compute zero-one loss on validation set
@@ -301,7 +273,7 @@ class ConvolutionalNeuralNetworkTrain(object):
                           # save best validation score and iteration number
                           best_validation_loss = this_validation_loss
                           self.best_params = self.params
-                      
+
                           # test it on the test set
                           test_losses = [self.test_model(i) for i in xrange(self.n_test_batches)]
                           test_score = numpy.mean(test_losses)
