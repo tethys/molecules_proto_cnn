1567b780e5ca988d528c14f13cfcea1cfe219eb2
viviana <petrescu.viviana@gmail.com>
2015-01-17 16:41:20 +0100

    reordered header files and imprved comments of constructor

--- proto_cnn/src/convolutional_nnet_train.py
+++ proto_cnn/src/convolutional_nnet_train.py
@@ -1,78 +1,80 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Oct 21 15:27:51 2014
-
 @author: vivianapetrescu
 """
 
-import time
-import numpy
-import os
-
 import datetime
 import logging
+import numpy
+import os
 import scipy
+import time
+
 import theano
 import theano.tensor as T
 
+import convolutional_neural_network_settings_pb2 as pb_cnn
+from google.protobuf import text_format
+from lenet_conv_pool_layer import LeNetConvPoolLayer
 from load_mitocondria import load_mitocondria
 from load_data_rescaled import load_mnist_data_rescaled
 from load_data import load_mnist_data
 from logistic_sgd import LogisticRegression
 from mlp import HiddenLayer
-from lenet_conv_pool_layer import LeNetConvPoolLayer
 
-import convolutional_neural_network_settings_pb2 as pb_cnn
-from google.protobuf import text_format
 
-# Make the network read this and setup
 class ConvolutionalNeuralNetworkTrain(object):
     """ The class takes a proto bufer as input, setups a CNN according to the
-        settings, trains the network and save the weights in a file
+        settings, trains the network and saves the weights in a file
     """
     def __init__(self, cnn_settings_protofile, cached_weights_file):
+        # Filename of the .npy file where the trained weights will be saved.
+	self.cached_weights_file = cached_weights_file
+        self.initialize_logger()
         # Create protobuff empty object
-        self.cached_weights_file = cached_weights_file
         settings = pb_cnn.CNNSettings();
         try:
            f = open(cnn_settings_protofile, "r")
            data = f.read()
-           text_format.Merge(data, settings);
+           text_format.Merge(data, settings)
            print "Network settings are \n"
            print data
            print "\n"
-
-           # Build up the ConvolutionalNeuralNetworkTrain model
-           self.create_layers_from_settings(settings);
-           self.initialize_logger()
+           # Build up the ConvolutionalNeuralNetworkTrain model from the layers description
+           self.create_layers_from_settings(settings)
 	   f.close();
         except IOError:
            print "Could not open file " + cnn_settings_protofile;
 
     def create_layers_from_settings(self, settings):
-        # Default values for optionl parameters
-        self.learning_rate = 1;
-        self.n_epochs = 5;
-        # This fields are required
-        self.dataset = 'mnist.pkl.gz' #settings.dataset;
-        self.batch_size = 50;
-        self.poolsize = 2;
-        
-        ##TODO add warning if default value is used
+        # Default values for optional parameters
+        self.learning_rate = 0.1;
+        self.n_epochs = 100;
+	self.batch_size = 100;
+	self.poolsize = 2;
+        # This fields are optional
+        ##TODO add colored warning if default value is used
         if settings.HasField('learning_rate'):
                 self.learning_rate = settings.learning_rate;
-        if settings.HasField('n_epochs'):
-                self.n_epochs = settings.n_epochs;
+        else:
+		print 'Warning - default learning rate ', self.learning_rate
+	if settings.HasField('n_epochs'):
+                self.n_epochs = settings.n_epochs
+	else:
+		print 'Warning - default number of epochs ', self.n_epochs
         if settings.HasField('batch_size'):
-                self.batch_size = settings.batch_size;
+                self.batch_size = settings.batch_size
+	else:
+		print 'Warning - default number of batch size ', self.batch_size
         if settings.HasField('poolsize'):
                 self.poolsize = settings.poolsize;
-
+	else:
+		print 'Warning - default number of poolsize ', self.poolsize
         # This fields are required
         self.dataset = 'mnist.pkl.gz' #settings.dataset;
 
-
-        # TODO this
+        # Add every convolutional layer to an array. Similarly for hidden layers.
         self.convolutional_layers = [];
         self.hidden_layers = [];
         for layer in settings.conv_layer:
@@ -83,8 +85,8 @@ class ConvolutionalNeuralNetworkTrain(object):
         # required at least one layer
         self.output_layer = settings.last_layer;
 
+	# Required parameters TODO still needed?
         self.dataset = settings.dataset
-        # required parameter
         self.cost_function = settings.cost_function;
 
 
@@ -175,7 +177,7 @@ class ConvolutionalNeuralNetworkTrain(object):
             hlayers.append(layer)
 
         # classify the values of the fully-connected sigmoidal layer
-        self.output_layer = LogisticRegression(input=layer_input, n_in= nbr_input, n_out=2)
+        self.output_layer = LogisticRegression(input=layer_input, n_in= nbr_input, n_out = self.output_layer.num_outputs)
 
         # the cost we minimize during training is the NLL of the model
         self.cost = self.output_layer.negative_log_likelihood(self.y)
