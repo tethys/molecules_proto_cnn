4d28f57723558220e9fe43e37455e5e2dcf7bbab
Viviana Petrescu <petrescu.viviana@gmail.com>
2015-02-03 15:59:38 +0100

    cleaned up test_voc_cnn and test_tp_cnn

--- proto_cnn/src/test_tp_cnn.py
+++ proto_cnn/src/test_tp_cnn.py
@@ -2,43 +2,34 @@
 """
 Created on Mon Nov 24 14:39:22 2014
 
-@author: vivianapetrescu
+@author: vpetresc
 """
 
-import convolutional_neural_network_settings_pb2 as pb_cnn
-import numpy as np
-import logging
-import datetime
-import os
-import time
-import theano.tensor as T
-import theano
-
-from google.protobuf import text_format
-
 from test_cnn import CNNTest
-from lenet_conv_pool_layer import LeNetConvPoolLayer
-from lenet_layer_conv_pool_non_symbolic import LeNetLayerConvPoolNonSymbolic
-from lenet_layer_conv_pool_separable_non_symbolic import LeNetLayerConvPoolSeparableNonSymbolic
-from logistic_sgd import LogisticRegression
-from mlp import HiddenLayer
 
 class CNNTestTP(CNNTest):
+    """ The class implements the methods for computing the batch
+        error using the error rate (TP+TN)/(all samples) """
 
     def compute_batch_error(self, batch_result_dict):
-	TP = batch_result_dict['TP']
-	FP = batch_result_dict['FP']
-	FN = batch_result_dict['FN']
-	TN = batch_result_dict['TN']
-        return (TP + TN)/float(TP + TN+ FP + FN) 
+        """ computes the error rate as the number of correctly 
+        classified samples"""
+        TP = batch_result_dict['TP']
+        FP = batch_result_dict['FP']
+        FN = batch_result_dict['FN']
+        TN = batch_result_dict['TN']
+        return (TP + TN)/float(TP + TN+ FP + FN)
+
     def compute_all_samples_error(self, all_samples_result):
-	TP = 0
-	FP = 0
-	FN = 0
-	TN = 0
-	for result in all_samples_result:
-		TP += result['TP']
-		FP += result['FP']
-		FN += result['FN']
-		TN += result['TN']
-	return (TP+TN)/float(TN+ TP + FP + FN)
+        """ Computes the all samples error by aggregating the
+            results from an array of dictionary results"""
+        TP = 0
+        FP = 0
+        FN = 0
+        TN = 0
+        for result in all_samples_result:
+            TP += result['TP']
+            FP += result['FP']
+            FN += result['FN']
+            TN += result['TN']
+        return (TP+TN)/float(TN+ TP + FP + FN)
--- proto_cnn/src/test_voc_cnn.py
+++ proto_cnn/src/test_voc_cnn.py
@@ -2,40 +2,28 @@
 """
 Created on Mon Nov 24 14:39:22 2014
 
-@author: vivianapetrescu
+@author: vpetresc
 """
 
-import convolutional_neural_network_settings_pb2 as pb_cnn
-import numpy as np
-import logging
-import datetime
-import os
-import time
-import theano.tensor as T
-import theano
-
-from google.protobuf import text_format
-
 from test_cnn import CNNTest
-from lenet_conv_pool_layer import LeNetConvPoolLayer
-from lenet_layer_conv_pool_non_symbolic import LeNetLayerConvPoolNonSymbolic
-from lenet_layer_conv_pool_separable_non_symbolic import LeNetLayerConvPoolSeparableNonSymbolic
-from logistic_sgd import LogisticRegression
-from mlp import HiddenLayer
 
 class CNNTestVOC(CNNTest):
+    """ Computes the VOC error """
 
     def compute_batch_error(self, batch_result_dict):
-	TP = batch_result_dict['TP']
-	FP = batch_result_dict['FP']
-	FN = batch_result_dict['FN']
-        return TP/float(TP + FP + FN) 
+        """ Computes the VOC error """
+        TP = batch_result_dict['TP']
+        FP = batch_result_dict['FP']
+        FN = batch_result_dict['FN']
+        return TP/float(TP + FP + FN)
+
     def compute_all_samples_error(self, all_samples_result):
-	TP = 0
-	FP = 0
-	FN = 0
-	for result in all_samples_result:
-		TP += result['TP']
-		FP += result['FP']
-		FN += result['FN']
-	return TP/float(TP + FP + FN)
+        """ Computes the error as VOC across al samples """
+        TP = 0
+        FP = 0
+        FN = 0
+        for result in all_samples_result:
+            TP += result['TP']
+            FP += result['FP']
+            FN += result['FN']
+        return TP/float(TP + FP + FN)
