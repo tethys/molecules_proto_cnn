09534cb1ceeae46e75a24b644e1b2395dc293a72
Viviana Petrescu <petrescu.viviana@gmail.com>
2015-02-03 16:16:46 +0100

    cleaned up train_cnn.py

--- proto_cnn/src/train_cnn.py
+++ proto_cnn/src/train_cnn.py
@@ -7,19 +7,10 @@ TODO consider using abstract base class
 http://zaiste.net/2013/01/abstract_classes_in_python/
 """
 
-import datetime
-import logging
-import numpy
-import os
-import scipy
-import time
-
-import theano
 import theano.tensor as T
+import numpy
 
 from base_cnn import CNNBase
-import convolutional_neural_network_settings_pb2 as pb_cnn
-from google.protobuf import text_format
 from lenet_conv_pool_layer import LeNetConvPoolLayer
 from logistic_sgd import LogisticRegression
 from mlp import HiddenLayer
@@ -30,15 +21,33 @@ class CNNTrain(CNNBase):
         settings, trains the network and saves the weights in a file
     """
     def __init__(self, protofile, cached_weights):
-	self.cnntype = 'TRAIN'
-	super(CNNTrain, self).__init__(protofile, cached_weights)
-    
+        self.cnntype = 'TRAIN'
+        super(CNNTrain, self).__init__(protofile, cached_weights)
+        self.train_set_x = None
+        self.train_set_y = None
+        self.valid_set_x = None
+        self.valid_set_y = None
+        self.test_set_x = None
+        self.test_set_y = None
+        self.n_train_batches = 0
+        self.n_valid_batches = 0
+        self.n_test_batches = 0
+        self.cost = 0
+        self.grads = None
+        self.params = None
+        self.output_layer = None
+        self.index = 0
+        self.input_shape = None
+        self.x = None
+        self.y = None
+
     def build_model(self):
-	# Fixed rng, make the results repeatable
+        """ Creates the actual model from the model settings."""
+        # Fixed rng, make the results repeatable
 
         datasets = self.load_samples()
 
-	# Train, Validation, Test 100000, 20000, 26... fot Mitocondria set
+        # Train, Validation, Test 100000, 20000, 26... fot Mitocondria set
         # Train, Validation, Test 50000, 10000, 10000 times 28x28 = 784 for MNIST dataset
         self.train_set_x, self.train_set_y = datasets[0]
         self.valid_set_x, self.valid_set_y = datasets[1]
@@ -58,14 +67,13 @@ class CNNTrain(CNNBase):
         self.n_valid_batches /= self.batch_size
         self.n_test_batches /= self.batch_size
 
-        #print('Size train %d, valid %d, test %d' % (self.train_set_x.shape.eval(), self.valid_set_x.shape.eval(), self.test_set_x.shape.eval())
-        print('Size train_batches %d, n_valid_batches %d, n_test_batches %d' % (self.n_train_batches, self.n_valid_batches, self.n_test_batches))
+        print 'Size train_batches %d, n_valid_batches %d, n_test_batches %d' % (self.n_train_batches, self.n_valid_batches, self.n_test_batches)
 
         # allocate symbolic variables for the data
         self.index = T.lscalar()  # index to a [mini]batch
         self.x = T.matrix('x')   # the data is presented as rasterized images
         self.y = T.ivector('y')  # the labels are presented as 1D vector of
-              		         # [int] labels
+                               # [int] labels
 
         ######################
         # BUILD ACTUAL MODEL #
@@ -73,56 +81,59 @@ class CNNTrain(CNNBase):
         print 'Building the model ...'
 
         # The input is an 4D array of size, number of images in the batch size, number of channels
-	# (or number of feature maps), image width and height.
-	nbr_feature_maps = 1
-	layer_input = self.x.reshape((self.batch_size, nbr_feature_maps, self.input_shape[0], self.input_shape[1]))
-        pooled_W = self.input_shape[0];
-        pooled_H = self.input_shape[1];
-	# Add convolutional layers followed by pooling
+        # (or number of feature maps), image width and height.
+        nbr_feature_maps = 1
+        layer_input = self.x.reshape((self.batch_size, nbr_feature_maps, self.input_shape[0], self.input_shape[1]))
+        pooled_width = self.input_shape[0]
+        pooled_height = self.input_shape[1]
+        # Add convolutional layers followed by pooling
         clayers = []
         for clayer_params in self.convolutional_layers:
             print 'Adding conv layer nbr filter %d, Ksize %d' % (clayer_params.num_filters, clayer_params.filter_w)
-            layer = LeNetConvPoolLayer(self.rng, input = layer_input,
-                                       image_shape=(self.batch_size, nbr_feature_maps, pooled_W, pooled_H),
+            layer = LeNetConvPoolLayer(self.rng, input=layer_input,
+                                       image_shape=(self.batch_size, nbr_feature_maps, pooled_width, pooled_height),
                                        filter_shape=(clayer_params.num_filters, nbr_feature_maps,
                                                      clayer_params.filter_w, clayer_params.filter_w),
                                        poolsize=(self.poolsize, self.poolsize))
             clayers.append(layer)
-            pooled_W = (pooled_W - clayer_params.filter_w + 1) / self.poolsize;
-            pooled_H = (pooled_H - clayer_params.filter_w + 1) / self.poolsize;
-            layer_input = layer.output;
-            nbr_feature_maps = clayer_params.num_filters;
+            pooled_width = (pooled_width - clayer_params.filter_w + 1) / self.poolsize
+            pooled_height = (pooled_height - clayer_params.filter_w + 1) / self.poolsize
+            layer_input = layer.output
+            nbr_feature_maps = clayer_params.num_filters
 
 
         # Flatten the output of the previous layers and add
-        # fully connected sigmoidal layers	
-        layer_input = layer_input.flatten(2);
-        nbr_input = nbr_feature_maps * pooled_W * pooled_H
+        # fully connected sigmoidal layers
+        layer_input = layer_input.flatten(2)
+        nbr_input = nbr_feature_maps * pooled_width * pooled_height
         hlayers = []
         for hlayer_params in self.hidden_layers:
             print 'Adding hidden layer fully connected %d' % (hlayer_params.num_outputs)
             layer = HiddenLayer(self.rng, input=layer_input, n_in=nbr_input,
-                         n_out = hlayer_params.num_outputs, activation=T.tanh)
-            nbr_input = hlayer_params.num_outputs;
+                                n_out=hlayer_params.num_outputs, activation=T.tanh)
+            nbr_input = hlayer_params.num_outputs
             layer_input = layer.output
             hlayers.append(layer)
 
         # classify the values of the fully-connected sigmoidal layer
-        self.output_layer = LogisticRegression(input=layer_input, n_in= nbr_input, n_out = self.last_layer.num_outputs)
+        self.output_layer = LogisticRegression(input=layer_input,
+                                               n_in=nbr_input,
+                                               n_out=self.last_layer.num_outputs)
 
         # the cost we minimize during training is the NLL of the model
         self.cost = self.output_layer.negative_log_likelihood(self.y)
 
         # create a list of all model parameters to be fit by gradient descent
         self.params = self.output_layer.params
-        for hl in reversed(hlayers):
-            self.params += hl.params
-        for cl in reversed(clayers):
-            self.params += cl.params
+        for hidden_layer in reversed(hlayers):
+            self.params += hidden_layer.params
+        for conv_layer in reversed(clayers):
+            self.params += conv_layer.params
 
 
         # create a list of gradients for all model parameters
         self.grads = T.grad(self.cost, self.params)
 
     def train_model(self):
-	raise NotImplementedError()
+        """ Abstract method to be implemented by subclasses"""
+        raise NotImplementedError()
